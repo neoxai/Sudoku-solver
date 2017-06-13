a39a923d699c2ab908e29192a13910e6fafdb589
John <neoxai@gmail.com>
2017-06-13 08:40:04 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sudoku_solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
